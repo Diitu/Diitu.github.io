a43b235b881f64f704497ff9b736942a88a4c259
Eray H. Özer <115875584+Diitu@users.noreply.github.com>
2024-07-11 14:58:25 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,17 +15,20 @@ document.getElementById('submission').addEventListener('submit', function(event)
             resubscribe: true
         })
     })
+        
     .then(response => {
-        if (response.ok) {
-            return response.json();
+        if (!response.ok) {
+            return response.json().then(errorData => {
+                throw new Error(`Network response was not ok: ${response.status} ${response.statusText} - ${JSON.stringify(errorData)}`);
+            });
         }
-        throw new Error('Network response was not ok:' + response.statusText);
+        return response.json();
     })
     .then(data => {
         alert('Subscription successful! You will be notified about the latest updates to Pulsey.');
     })
     .catch(error => {
-        console.error('There was a problem with your submission:', error.message);
+        console.error('There was a problem with your submission:', error);
         alert('There was a problem with your submission: ' + error.message);
     });
 });
