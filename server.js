594d752fb9fe6a714cbdd2670308a505d689ad6c
Eray H. Özer <115875584+Diitu@users.noreply.github.com>
2024-07-11 14:51:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ document.getElementById('submission').addEventListener('submit', function(event)
 
     const email = document.getElementById('email').value;
 
-    fetch('https://connect.mailerlite.com/api/pulsey_api', {
+    fetch('https://connect.mailerlite.com/api/pulsey', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'X-MailerLite-ApiKey': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiMGFlMTNlNzFlNzczY2I4OWMyNzA4OTBlMDcwZTJiYzEwNjAwMzZmZDQ0ZWE3M2YxZGMxOTk0OGFjYmMyMjVmMmY3NWE0NDE3MzgxZGNlNTIiLCJpYXQiOjE3MjA2OTQ5NjIuNDQ2NjEyLCJuYmYiOjE3MjA2OTQ5NjIuNDQ2NjE0LCJleHAiOjQ4NzYzNjg1NjIuNDQ0MDMsInN1YiI6IjEwMjkwMDYiLCJzY29wZXMiOltdfQ.VbjsLblPx5rd-CF3WzqVICda0_l4Qd5wO_DZRUjw_hZQsHemNA1MZV01_xL1QXuO-GnJQh5HwxxqjWD8aI7FXhyWg17O3E5XvjFdICBTYAIt36trbPZ4z4bRkXupmrjKUDTsdsxhjBKP1JMYaZ78lqeURdSai4-Dw9YZGXh67Av-_lNCHrZU0iaWkRGuA1hkuBcuB-SUYYPl23zm7pYtu89jdELnCdpnJakPtUdM8ZOHN4o0m0T6L8Rj2xe182N8MdCkk-mIDdEFAXoGIRHbgNoUyMiCVBdyNjiiqMecUiEDPjcIZoQauSDgjprmZ_bk9gUkr1KpVM_cacnpcDZWw0pQevNf3cbcsxZ__CjDYQeas6-bhJ35fTfDAPBL459SbC-ThLS7EwPYZrWcSsiy8KJdHaFP4YB4OLHmJTuO9BSZ_zjfRvVe0XLdG8vf4-GVbHPo1YLjV5tXA6eu7QG7gqt-z-ysJFv1fyS-wDcN_j7TK5QAvBFAM7bfl3vaix3EUn3qX1-2OgHBBB6HVxan2f-QX0e88mOV7oCyzaKgOy5rxboUlx7lodzL2L9f_WJDbgnQpUOtiyjNVjshsKrv-rzusQ1cCs24maw4xsYiAIW-_7-IKA2asbxj2BW9e1l8itw_6OlS9uQCjZDDZ20FfPxwn1cOqaFkzFhVmvsQWYQ'
+            'X-MailerLite-ApiKey': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiZGI5ZmQ2MGVkNWZmNTNiMjk5YTE1N2Y4MDM3NDY0YTcwNTcxNTI3ZjA5MzU4ZTFlMDc1NzI2OWI2ZWI4MDhkNDYzZDMyNjYwOWQ5ODVkNDUiLCJpYXQiOjE3MjA2OTg2MTcuMTU5MDgzLCJuYmYiOjE3MjA2OTg2MTcuMTU5MDg1LCJleHAiOjQ4NzYzNzIyMTcuMTU2MzM3LCJzdWIiOiIxMDI5MDA2Iiwic2NvcGVzIjpbXX0.rR5Ql0TDI4sPzQIJmxGMQRm8Hsy976r1hFX8jIBALVOBZbgToQbObFqiUvkbBlWpo3pf36pTWDiX54xZBTfwz81BSYWL4cfAV7jB-_OeHsfKVxfGIs46_1Pxv4XfNCgF0c_H7_JI5qkQaxfj6FlCf1mMh88gQxr3ivF0pL3OSkrk9mIrHGy_4fkQlEe56hb0lAEsGFacA7llRp9ktM8L-jEBSssLFMjcW1R7JlP3lR2tyO3q4vbrBvilF9CopOx4Zst5QP0S-QnEMORoybtrlTsugMUHNaBJWwNY83CWQmt8yTnDJk0-S2VzAyRIgd9VQuZn0DOHUIL_PcVI4k460K9ueXXin7kI7dii3jkxwbDJZLnOIyCziE285O6tbnVHt96mxQDmxmNDGYV3d1Pel10grF7SaP09VCLC4N6X0_fV6DiRi8yA-f1eI4bLZ3BCX9SbViclvb0WSYRI09Ohbhp3vCMLgxRAp1T9OZQU3HQSG4E58QUGJJO0P1wOQ3DJ9RsiZJaYiu8eJ_pCnDCluZOXX0KzQriSB-5kRp1FbipMrfAqa45aXBNYbVPGBQy_CnZuOd3irzEGiive438Z18jBWD5btzGwyQs25iFs16KPTMgrsJVYKKmbc82f7y5zPlD7fFk1L1IaIKdEK_RakAGL3_u8ytmTpZ7k80PVMO8'
         },
         body: JSON.stringify({
             email: email,
